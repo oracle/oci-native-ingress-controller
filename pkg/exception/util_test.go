6dbeef1259c7c17ffd248604e27cdee24fef2c79
Piyush Tiwari <piyush.s.tiwari@oracle.com>
2025-04-21 10:57:47 +0530

    add unit tests to exception and certificatecleanup packages

--- pkg/exception/util_test.go
+++ pkg/exception/util_test.go
@@ -7,6 +7,37 @@ import (
 	"testing"
 )
 
+func TestNotFoundServiceError(t *testing.T) {
+	RegisterTestingT(t)
+
+	notfoundServiceError := NotFoundServiceError{}
+	Expect(notfoundServiceError.GetHTTPStatusCode()).To(Equal(404))
+	Expect(notfoundServiceError.GetMessage()).To(Equal("NotFound"))
+	Expect(notfoundServiceError.GetCode()).To(Equal("NotFound"))
+	Expect(notfoundServiceError.GetOpcRequestID()).To(Equal("fakeopcrequestid"))
+	Expect(notfoundServiceError.Error()).To(Equal("NotFound"))
+}
+
+func TestConflictServiceError(t *testing.T) {
+	RegisterTestingT(t)
+
+	conflictServiceError := ConflictServiceError{}
+	Expect(conflictServiceError.GetHTTPStatusCode()).To(Equal(409))
+	Expect(conflictServiceError.GetMessage()).To(Equal("Conflict"))
+	Expect(conflictServiceError.GetCode()).To(Equal("Conflict"))
+	Expect(conflictServiceError.GetOpcRequestID()).To(Equal("fakeopcrequestid"))
+	Expect(conflictServiceError.Error()).To(Equal("Conflict"))
+}
+
+func TestTransientError(t *testing.T) {
+	RegisterTestingT(t)
+
+	err := errors.New("test-error")
+	transientErr := NewTransientError(err)
+	Expect(transientErr.GetWrappedError()).To(Equal(err))
+	Expect(transientErr.Error()).To(Equal(err.Error()))
+}
+
 func TestHasTransientError(t *testing.T) {
 	RegisterTestingT(t)
 
