e768d39c31385c747329211bf30eeccf6e5dd880
jesse.millan <jesse.millan@oracle.com>
2026-05-19 17:24:55 -0700

    Count backend reconciliation in ingress sync metrics.
    Signed-off-by: jesse.millan <jesse.millan@oracle.com>

--- pkg/controllers/backend/backend.go
+++ pkg/controllers/backend/backend.go
@@ -19,6 +19,7 @@ import (
 	"k8s.io/client-go/tools/events"
 
 	"github.com/oracle/oci-native-ingress-controller/pkg/client"
+	"github.com/oracle/oci-native-ingress-controller/pkg/metric"
 	"k8s.io/klog/v2"
 
 	ociloadbalancer "github.com/oracle/oci-go-sdk/v65/loadbalancer"
@@ -51,8 +52,9 @@ type Controller struct {
 	saLister           corelisters.ServiceAccountLister
 	eventRecorder      events.EventRecorder
 
-	queue  workqueue.RateLimitingInterface
-	client *client.ClientProvider
+	queue            workqueue.RateLimitingInterface
+	client           *client.ClientProvider
+	metricsCollector *metric.IngressCollector
 }
 
 func NewController(
@@ -64,6 +66,7 @@ func NewController(
 	endpointLister corelisters.EndpointsLister,
 	podLister corelisters.PodLister,
 	client *client.ClientProvider,
+	metricsCollector *metric.IngressCollector,
 	eventRecorder events.EventRecorder,
 ) *Controller {
 
@@ -78,6 +81,7 @@ func NewController(
 		client:             client,
 		eventRecorder:      eventRecorder,
 		queue:              workqueue.NewRateLimitingQueue(workqueue.NewItemExponentialFailureRateLimiter(10*time.Second, 5*time.Minute)),
+		metricsCollector:   metricsCollector,
 	}
 
 	return c
@@ -104,6 +108,17 @@ func (c *Controller) processNextItem() bool {
 
 // sync is the business logic of the controller.
 func (c *Controller) sync(key string) error {
+	if c.metricsCollector != nil {
+		c.metricsCollector.IncrementSyncCount()
+	}
+
+	startSyncTime := time.Now()
+	defer func() {
+		if c.metricsCollector != nil {
+			c.metricsCollector.AddIngressBackendSyncTime(time.Since(startSyncTime).Seconds())
+		}
+	}()
+
 	namespace, name, err := cache.SplitMetaNamespaceKey(key)
 	if err != nil {
 		klog.ErrorS(err, "Failed to split meta namespace cache key", "cacheKey", key)
